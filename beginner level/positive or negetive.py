d8790e2a861fec69deff9789c7ca54a1e7fe7884
vetrivelmnv13 <37337095+vetrivelmnv13@users.noreply.github.com>
2018-03-19 16:29:58 +0530

    Create positive or negetive.py

---
+++ beginner level/positive or negetive.py
@@ -0,0 +1,11 @@
+ch=str(input('enter a value'))
+if((ch>='a' and ch<='z')or(ch>='A' and ch<='Z')):
+  print("not valid")
+else:
+  b=float(ch)
+  if((0<b)and(b<=100000)):
+    print("positive")
+  elif(b<0):
+    print("negetive")
+  else:
+    print("zero")

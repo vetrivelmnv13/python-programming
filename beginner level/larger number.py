c617ae804982a80bb6aaa0f874f2076856d269c0
vetrivelmnv13 <37337095+vetrivelmnv13@users.noreply.github.com>
2018-03-20 10:19:19 +0000

    Create larger number.py

---
+++ beginner level/larger number.py
@@ -0,0 +1,9 @@
+a=int(input("enter the input:"))
+b=int(input("enter the input:"))
+c=int(input("enter the input"))
+if(a>b and a>c):
+  print("a is large")
+elif(b>a and b>c):
+  print("b is large")
+else:
+  print("c is large")

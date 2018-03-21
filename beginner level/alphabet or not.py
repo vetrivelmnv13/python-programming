22e59bf2619b8510397f8f2d431c1c899cd6c77b
vetrivelmnv13 <37337095+vetrivelmnv13@users.noreply.github.com>
2018-03-21 10:52:14 +0530

    Update alphabet or not.py

--- beginner level/alphabet or not.py
+++ beginner level/alphabet or not.py
@@ -1,5 +1,5 @@
 ch=input("enter the chracter")
-if((ch>="a" and ch<="z") or (ch>"A" and ch<="Z")):
+if((ch>="a" and ch<="z") or (ch>="A" and ch<="Z")):
   print("a charecter is an alphabet")
 else:
   print("not a alphabet")

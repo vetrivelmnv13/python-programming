91448b9b377bfacd3a1a446c5bd94da0baf3f5a6
vetrivelmnv13 <37337095+vetrivelmnv13@users.noreply.github.com>
2018-04-28 10:25:21 +0530

    Create prime or not.py

---
+++ beginner level/prime or not.py
@@ -0,0 +1,11 @@
+num=int(input("enter the positive number"))
+if num>1:
+  for i in range(2,num):
+    if(num%i)==0:
+      print("no")
+      break
+  else:
+    print("yes")
+else:
+  print("no")
+      

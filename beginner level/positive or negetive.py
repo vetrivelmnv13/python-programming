6c4b1c6f489afec0ffc6c0c701986849aa2c8fe1
vetrivelmnv13 <37337095+vetrivelmnv13@users.noreply.github.com>
2018-07-11 23:18:14 -0700

    Update positive or negetive.py

--- beginner level/positive or negetive.py
+++ beginner level/positive or negetive.py
@@ -1,11 +1,7 @@
-ch=str(input('enter a value'))
-if((ch>='a' and ch<='z')or(ch>='A' and ch<='Z')):
-  print("not valid")
+a=int(input())
+if a>0:
+  print("Positive")
+elif a<0:
+  print("Negetive")
 else:
-  b=float(ch)
-  if((0<b)and(b<=100000)):
-    print("positive")
-  elif(b<0):
-    print("negative")
-  else:
-    print("zero")
+  print("Zero")
